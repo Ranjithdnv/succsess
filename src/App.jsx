a12bc4e93db07eadcbb8a1928ae1eec3ac5b02ad
Ranjithdnv <rith8596@gmail.com>
2023-08-09 18:11:53 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+// import { useState}from "react"
+// import { VideoRoom } from './compponents/VideoRoom';
+function App() {
+  
+   return(
+    
+   <div className='bodyzz'><div className='child'>
+   <div style={{fontWeight:"larger"}}>SUCCESSFUL PAYMENT</div>
+   <div>ENJOY YOUR BUYING</div>
+   <div>  <a href = "https://paymentarzor.onrender.com" >home</a></div>
+  
+ </div></div>
+   )
+}
+
+export default App;
